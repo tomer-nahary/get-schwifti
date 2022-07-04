c4b950e532613a2e19bc18b1003508a64f1c7ef2
Tomer Nahary <tomer.nahary@gmail.com>
2022-07-04 10:55:12 +0300

    Add switch tiles ability with boardManager

--- boardDisplayer.js
+++ boardDisplayer.js
@@ -2,22 +2,38 @@ class BoardDisplayer {
     #rootNode;
     constructor(rootNode) {
         this.#rootNode = rootNode;
-        console.log(this.#rootNode);
     }
-    display(board) {
+    display(boardManager) {
+        const board = boardManager.board;
+        this.#rootNode.innerHTML = "";
         for (let i = 0; i < board.rowsNumber; i++) {
             let rowElement = document.createElement("div");
             rowElement.classList.add("board-row");
             this.#rootNode.append(rowElement);
             for (let j = 0; j < board.length / board.rowsNumber; j++) {
-                let tileNumber = board.tiles[i * board.rowsNumber + j];
-                let tileElement = document.createElement("div");
+                const tileIndex = i * board.rowsNumber + j;
+                const tileNumber = board.tiles[tileIndex];
+                const tileElement = document.createElement("div");
                 tileElement.classList.add("board-tile");
-                tileElement.id = `board-tile-${tileNumber}`
+                tileElement.id = getTileId(tileIndex);
                 tileElement.innerText = tileNumber;
+                tileElement.addEventListener("click", () => this.#onTileClicked(boardManager, tileIndex))
                 rowElement.append(tileElement);
             }
         }
         board.rowsNumber
     }
+    #onTileClicked(boardManager, tileIndex) {
+        let [index1, index2] = boardManager.onTileClick(tileIndex);
+        this.#switchTiles(index1, index2);
+    }
+    #switchTiles(index1, index2) {
+        const tile1 = document.getElementById(getTileId(index1));
+        const tile2 = document.getElementById(getTileId(index2));
+        const temp = tile1.innerText;
+        tile1.innerText = tile2.innerText;
+        tile2.innerText = temp;
+    }
 }
+
+const getTileId = (tileIndex) => `board-tile-${tileIndex}`;
