f37eed17028313a447b965b52a4a4c9583b59d91
Tomer Nahary <tomer.nahary@gmail.com>
2022-07-04 09:43:24 +0300

    Add rows number to board

--- JS/board.js
+++ JS/board.js
@@ -1,6 +1,7 @@
 class Board {
-    constructor(tiles, length) {
+    constructor(tiles, length, rowsNumber) {
         this.tiles = tiles;
         this.length = length;
+        this.rowsNumber = rowsNumber;
     }
 }
