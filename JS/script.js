82bead3413c88e08e1a8b3fca24f9afbb43edf47
Tomer Nahary <tomer.nahary@gmail.com>
2022-07-04 15:38:36 +0300

    Use size picker in script

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,20 @@
-let boardElement = document.getElementById("board");
-let boardGenerator = new BoardGenerator();
-let board = boardGenerator.generateBoard(4);
-let winnerAnnouncer = new WinnerAnnouncer();
-if (board) {
-    let boardManager = new BoardManager(board, winnerAnnouncer)
-    let boardDisplayer = new BoardDisplayer(boardElement);
-    boardDisplayer.display(boardManager);
+const generateButton = document.getElementById("generate-button");
+const boardSizeElement = document.getElementById("board-size");
+const increaseElement = document.getElementById("increase-size")
+const decreaseElement = document.getElementById("decrease-size")
+const boardElement = document.getElementById("board");
 
+const boardGenerator = new BoardGenerator();
+const winnerAnnouncer = new WinnerAnnouncer();
+const sizePicker = new SizePicker(increaseElement, decreaseElement, boardSizeElement);
+const boardDisplayer = new BoardDisplayer(boardElement);
+
+
+function generateBoard() {
+    winnerAnnouncer.reset();
+    const board = boardGenerator.generateBoard(sizePicker.getSize());
+    const boardManager = new BoardManager(board, winnerAnnouncer)
+    boardDisplayer.display(boardManager);
 }
+generateButton.addEventListener("click", generateBoard);
+
